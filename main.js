0349defa4673114e3379d77f66abf30093cdbda5
vnod96 <vinod.suthar16@gmail.com>
2018-10-20 17:24:07 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+var color="#de2420";
+displayBlock(1);
+
+$('#next1btn').click(function(){
+  displayBlock(2);
+});
+$('#next2btn').click(function(){
+  displayBlock(3);
+});
+$('#prev2btn').click(function(){
+  displayBlock(1);
+});
+$('#prev3btn').click(function(){
+  displayBlock(2);
+});
+
+
+
+
+function displayBlock(i){
+  if(i==1){
+    $('#firstpage').css('color',color);
+    $('#secondpage').css('color','grey');
+    $('#thirdpage').css('color','grey');
+    $('#firstpagediv').show();
+    $('#secondpagediv').hide();
+    $('#thirdpagediv').hide();
+  }
+  if(i==2){
+    $('#firstpage').css('color','grey');
+    $('#secondpage').css('color',color);
+    $('#thirdpage').css('color','grey');
+    $('#firstpagediv').hide();
+    $('#secondpagediv').show();
+    $('#thirdpagediv').hide();
+  }
+  if(i==3){
+    $('#firstpage').css('color','grey');
+    $('#secondpage').css('color','grey');
+    $('#thirdpage').css('color',color);
+    $('#firstpagediv').hide();
+    $('#secondpagediv').hide();
+    $('#thirdpagediv').show();
+  }
+}
